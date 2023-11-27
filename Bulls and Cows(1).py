fb52d2bd12ec3aef12c8ed4f71cd8081ef76a93c
Aditya Jain <adityaastro.id2004@gmail.com>
2023-11-27 11:01:44 +0530

    Update Bulls and Cows(1).py
    Improves the efficiency and removes the use of global variables

--- Bulls and Cows(1).py
+++ Bulls and Cows(1).py
@@ -1,62 +1,52 @@
-print(''' 																BULLS AND COWS
-RULES:-
-1) YOU HAVE TO GUESS A 4 DIGIT NUMBER
-2) IF THE DIGIT OF YOUR NUMBER MATCHES THE DIGIT IN THE COMPUTER GENERATED NUMBER BUT NOT IN THE RIGHT PLACE, IT WILL BE A COW.
-3) IF YOUR NUMBER HAS A DIGIT AT SAME PLACE AS THE COMPUTER GENERATED NUMBER IT WILL BE A BULL
-4) THE LOWER CHANCES YOU TAKE TO GUESS THE NUMBER, THE BETTER
-5) ALSO REMEMBER ALL THE DIGITS OF THE NUMBER ARE DIFFERENT''')
-print()
+import random
 
 
-def number():
-	import random
-	a = random.randint(1000, 9999)
-	a = str(a)
-	b = list(a)
-	global chance
-	chance = 0
-	for j in range(len(b) - 1):
-		if b.count(b[j]) > 1:
-			return number()
-
-	def game():
-		global b
-		t = list(a)
-		b = t
-	#	print(t)
-		global chance
-
-		while True:
-			_input = str(input('Guess the number: '))
-			t_input = list(_input)
-			count_b = 0
-			count_c = 0
-			chance += 1
-			if t_input == t:
-				print('congratulations!! you guessed correctly in', chance, 'chances')
-				enter = input('If you want to play again press "1" otherwise press any other key: ')
-				if enter == '1':
-					print()
-					return number()
-				else:
-					break
-			elif len(t_input) != 4:
-				print('Enter a 4 digit number')
-			else:
-
-				for i in range(len(t)):
-					if t_input.count(t_input[i]) > 1:
-						print('All the numbers should be different')
-						return game()
-					elif t_input[i] == t[i]:
-						count_b += 1
-					elif t_input[i] in t and t_input[i] != t[i]:
-						count_c += 1
-
-				print('your guessed number has ', count_b, 'bulls and ', count_c, 'cows')
-				return game()
-
-	game()
+def generate_number():
+    while True:
+        number = random.sample(range(10), 4)
+        if number[0] != 0:
+            return ''.join(map(str, number))
 
 
-number()
+def play_game(secret_number):
+    chances = 0
+
+    while True:
+        user_input = input('Guess the number: ')
+
+        if not user_input.isdigit() or len(user_input) != 4:
+            print('Please enter a valid 4-digit number.')
+            continue
+
+        chances += 1
+        bulls = sum(user_digit == secret_digit for user_digit, secret_digit in zip(user_input, secret_number))
+        cows = sum(user_digit in secret_number and user_digit != secret_digit for user_digit, secret_digit in
+                   zip(user_input, secret_number))
+
+        if bulls == 4:
+            print(f'Congratulations!! You guessed correctly in {chances} chances.')
+            play_again = input('If you want to play again, press "1". Otherwise, press any other key: ')
+            if play_again != '1':
+                break
+            else:
+                secret_number = generate_number()
+                chances = 0
+        else:
+            print(f'Your guessed number has {bulls} bulls and {cows} cows.')
+
+
+def main():
+    print('BULLS AND COWS GAME')
+    print('RULES:')
+    print('1) Guess a 4-digit number.')
+    print('2) Bulls: Right digit in the right place.')
+    print('3) Cows: Right digit, but in the wrong place.')
+    print('4) All digits must be different.')
+    print()
+
+    secret_number = generate_number()
+    play_game(secret_number)
+
+
+if __name__ == "__main__":
+    main()
